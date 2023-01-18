9d8f8bef413e87e6f61cb4afa4315a92f4659e27
Metranohm <andrewwink@gmail.com>
2023-01-18 00:04:44 -0600

    contact function

---
+++ app.js
@@ -0,0 +1,11 @@
+// Get the "Contact" link from the navbar
+let contactLink = document.querySelector("nav a[href='#contact']");
+
+// Add a click event listener to the link
+contactLink.addEventListener("click", function() {
+  // Get the "Contact" section
+  let contactSection = document.querySelector("section#contact");
+
+  // Change the text of the section
+  contactSection.innerHTML = "Thank you for your interest! Our contact information is currently being updated.";
+});
